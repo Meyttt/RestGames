61a529be72ef7659e617ff6fda29eed6d2ee9d75
a.chebotareva <sabermeyt@gmail.com>
2018-06-25 22:23:03 +0300

    Рабочий рест

--- src/main/java/MessageRestService.java
+++ src/main/java/MessageRestService.java
@@ -1,3 +1,5 @@
+import org.apache.log4j.Logger;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -9,6 +11,12 @@ public class MessageRestService {
     @Path("/{param}")
     public Response getResponse(@PathParam("param") String msg){
         String res = msg==null || msg.length()==0 ? "Random - Random" : msg+ " - Random";
+        return Response.status(200).entity(res).build();
+    }
+    @GET
+    @Path("/")
+    public Response getResponse(){
+        String res = "Random - Random";
         return Response.status(200).entity(res).build();
     }
 }
